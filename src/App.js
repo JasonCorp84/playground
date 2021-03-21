1e24361420c1f0507f3e53d610b4445f5d8cb0e1
Csaba Balogh <csababalogh84@gmail.com>
2021-03-21 10:10:25 -0700

    playground initiated

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Container from './components/Bars'
+
+function App() {
+  return (
+    <div className="App">
+      <div className='App-container'>
+        <Container />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Bars/Container.jsx
@@ -0,0 +1,11 @@
+import react from 'react';
+
+const Container = () => {
+  return (
+    <div>
+      bar container...
+    </div>
+  )
+}
+
+export default Container;
